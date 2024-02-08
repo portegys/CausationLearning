a5de3d0bd5bc6e75af94bb53e68624d8f207c0e1
portegys <portegys@gmail.com>
2024-02-07 23:34:58 -0600

    Decision tree plot

--- src/nn/causation_test_results_decision_tree.py
+++ src/nn/causation_test_results_decision_tree.py
@@ -8,7 +8,6 @@ from sklearn import metrics
 from sklearn.tree import export_graphviz
 from sklearn.externals.six import StringIO 
 import pandas
-import graphviz
 import sys, getopt
 from IPython.display import Image
 import pydotplus
@@ -39,38 +38,33 @@ if input_filename == '':
 
 # Read test results dataset
 dataset = pandas.read_csv(input_filename)
-dataset = dataset.rename(columns={"test_pct": "Score"})
+dataset = dataset.rename(columns={'NUM_EVENT_TYPES': 'NET','NUM_CAUSATIONS': 'NC','MAX_CAUSE_EVENTS': 'MCE','MAX_INTERVENING_EVENTS': 'MIE', 'test_pct': 'Score'})
+X_names = ['NET','NC','MCE','MIE']
 y_name = ['Score']
-X_names = ['NUM_EVENT_TYPES','NUM_CAUSATIONS','MAX_CAUSE_EVENTS','MAX_INTERVENING_EVENTS']
 
 # Average accuracy for runs with same parameters.
 X_ave = dataset.filter(items=X_names).drop_duplicates()
 X_ave[['Score']] = 0
 X_ave = X_ave.reset_index()
 for index, row in X_ave.iterrows():
-  scores = dataset.loc[dataset['NUM_EVENT_TYPES'] == row['NUM_EVENT_TYPES']]
-  scores = scores.loc[dataset['NUM_CAUSATIONS'] == row['NUM_CAUSATIONS']]
-  scores = scores.loc[dataset['MAX_CAUSE_EVENTS'] == row['MAX_CAUSE_EVENTS']]
-  scores = scores.loc[dataset['MAX_INTERVENING_EVENTS'] == row['MAX_INTERVENING_EVENTS']]['Score']
-  print(scores)
+  scores = dataset.loc[dataset['NET'] == row['NET']]
+  scores = scores.loc[dataset['NC'] == row['NC']]
+  scores = scores.loc[dataset['MCE'] == row['MCE']]
+  scores = scores.loc[dataset['MIE'] == row['MIE']]['Score']
   if len(scores) > 0:
     row['Score'] = sum(scores) / len(scores)
 dataset = X_ave
 X = dataset[X_names]
 y = dataset[y_name]
 
-# Score as A, B, C, D, F
+# Score as Good, Fair, Poor
 def score(x):
   if float(x) >= 90.0:
-    return 'A'
-  elif float(x) >= 80.0:
-    return 'B'
+    return 'Good'
   elif float(x) >= 70.0:
-    return 'C'
-  elif float(x) >= 60.0:
-    return 'D'
+    return 'Fair'
   else:
-    return 'F'
+    return 'Poor'
 
 y_scores = [score(i) for i in dataset['Score']]
 dataset['Score'] = y_scores
@@ -83,33 +77,22 @@ model = clf.fit(X, y)
 # Predict the response for test dataset
 y_pred = model.predict(X)
 
-# Model Accuracy, how often is the classifier correct?
+# Accuracy
 print("Accuracy:",metrics.accuracy_score(y, y_pred))
 
 # Text representation.
-text_representation = tree.export_text(clf)
+print('Text results:')
+text_representation = tree.export_text(clf, feature_names=['NUM_EVENT_TYPES','NUM_CAUSATIONS','MAX_CAUSE_EVENTS','MAX_INTERVENING_EVENTS'])
 print(text_representation)
 
-fig = plt.figure(figsize=(25,20))
-_ = tree.plot_tree(clf, 
-                   feature_names=X_names,
-                   class_names=['A','B','C','D','F'],
-                   filled=True)
-
-# DOT data
-dot_data = tree.export_graphviz(clf, out_file=None,
-                                feature_names=X_names,
-                                class_names=['A','B','C','D','F'],
-                                filled=True)
-
-# Draw graph
-graph = graphviz.Source(dot_data, format="png")
-graph
-
+# Graphical representation.
+print('Writing graphical results to causation_test_results_decision_tree.png')
 dot_data = StringIO()
-export_graphviz(clf, out_file=dot_data,  
-                filled=True, rounded=True,
-                special_characters=True,feature_names = X_names,class_names=['A','B','C','D','F'])
-graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
-graph.write_png('decision_tree.png')
+export_graphviz(clf, out_file=dot_data,
+                filled=True, rounded=True, impurity=False,
+                special_characters=True,
+                feature_names = X_names,
+                class_names=['Good', 'Fair', 'Poor'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('causation_test_results_decision_tree.png')
 Image(graph.create_png())
